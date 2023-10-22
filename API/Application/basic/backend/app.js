e129f66d69fa60cfab3961724cd198764849095a
__Aditya__ <aditya.219301222@muj.manipal.edu>
2023-10-22 15:28:23 +0530

    Sixth Commit

---
+++ API/Application/basic/backend/app.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const multer = require('multer')
+// const { v4: uuidv4 } = require('uuid')
+
+const path = require('path')
+
+const feedRoutes = require('./routes/feed')
+const authRoutes = require('./routes/auth')
+
+const app = express()
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        file.originalname.replaceAll("#", "_")
+        file.originalname.replaceAll(" ", "_")
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'images/jfif'
+    ) {
+        cb(null, true)
+    }
+    else {
+        cb(null, false)
+    }
+}
+
+app.use(bodyParser.json())
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+app.use('/feed', feedRoutes)
+app.use('/auth', authRoutes)
+
+app.use((error, req, res, next) => {
+    console.log(error)
+    const status = error.statusCode || 500
+    const message = error.message
+    const data = error.data
+    res
+        .status(status)
+        .json({
+            message: message,
+            data: data
+        })
+})
+
+mongoose
+    .connect('mongodb+srv://aditya-0011:UaKTgAjKESgT0pMk@cluster0.gdrorud.mongodb.net/social-media')
+    .then(result => {
+        console.log("Connected")
+        app.listen(1010)
+    })
+    .catch(e => console.log(`Db Connection Error${e}`))
